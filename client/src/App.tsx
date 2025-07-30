fccc030b8e0c1e833dc2049a54deaaa68ac5a5d2
firebolt778 <firebolt77800@gmail.com>
2025-07-30 18:23:21 +0900

    Add scroll-to-top functionality on route change in Router component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
-import { Switch, Route } from "wouter";
+import { useEffect } from "react";
+import { Switch, Route, useLocation } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -15,6 +16,12 @@ import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
 
 function Router() {
+  const [location] = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
